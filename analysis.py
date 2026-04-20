5df8489cb6bbdea237740d5520ff4ea6e083f405
satvikablr-pixel <satvika.blr@gmail.com>
2026-04-20 23:19:23 +0530

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -2,8 +2,10 @@ import pandas as pd
 
 df = pd.read_csv("data.csv")
 print(df)
+
 print("Average:", df["Marks"].mean())
 print("Max:", df["Marks"].max())
 print("Min:", df["Marks"].min())
+
 topper = df[df["Marks"] == df["Marks"].max()]
 print(topper)
